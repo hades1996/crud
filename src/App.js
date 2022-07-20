be71b0ada19d82418232e3b8f89a6449fd2e2257
hades1996 <48794592+hades1996@users.noreply.github.com>
2022-07-20 18:56:03 -0500

    video 15

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 
 import React,{ useState} from 'react'
 import { isEmpty } from 'lodash';
+import shortid from 'shortid';
 
 function App() {
   const [task, setTask] = useState("")
+  const [tasks, setTasks] = useState([])
+
   const addTask=(e)=>{
     e.preventDefault()
       if (isEmpty(task)){
         console.log("Task empety")
         return
       }
-    console.log("ok")
+
+      const newTask={
+        id: shortid.generate(),
+        name: task
+      }
+
+      setTasks([...tasks,newTask])
+    
     setTask("")
   }
+
   return (
     <div className='container mt-5'>
       <h1>tareas</h1>
@@ -21,11 +32,15 @@ function App() {
         <div className='col-8'>
           <h4 className='text-center'>lista de tareas</h4>
           <ul className='list-group'>
-            <li className='list-group-item'>
-              <span className='lead'>nombre de la tarea</span>
+            {
+              tasks.map((task)=>(
+            <li className='list-group-item' key={task.id}>
+              <span className='lead'>{task.name}</span>
               <button className='btn btn-danger btn-sm float-right mx-2'>eliminar</button>
               <button className='btn btn-warning btn-sm float-right'>editar</button>
             </li>
+            ))
+            }
           </ul>
         </div>
         <div className='col-4'> 
